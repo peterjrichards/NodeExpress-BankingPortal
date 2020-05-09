2b333e4c12638ee44bf17ce9a60f1ec5e6de634f
Peter Richards <peter.richards@thefuturetoday.net>
2020-05-09 14:17:28 +0100

    Completed module

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.render('index', { title: 'Index' });
 });
