3805455c8915fe67e8fc94824dd4ddbc2ca106ec
Peter Richards <peter.richards@thefuturetoday.net>
2020-05-09 14:16:27 +0100

    Completed module

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const port = 3000;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.render('index', { title: 'Index' });
+});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
